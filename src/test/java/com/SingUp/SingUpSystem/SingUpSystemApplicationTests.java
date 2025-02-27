884ce3c8aac9de9e65d120d22023561b548fa64c
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-02-27 20:33:31 -0300

    Registration System

---
+++ src/test/java/com/SingUp/SingUpSystem/SingUpSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SingUp.SingUpSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingUpSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
